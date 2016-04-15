52db38d8ffd679c1aebd9f17c0b79174f7a4317a
Nicolás Palmieri <npalmieri@humana.com>
2016-04-15 11:15:45 -0400

    project started

---
+++ cedarExample/cedarExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cedarExample
+//
+//  Created by Nico on 4/15/16.
+//  Copyright © 2016 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cedarExample/cedarExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  cedarExample
+//
+//  Created by Nico on 4/15/16.
+//  Copyright © 2016 Globant. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
