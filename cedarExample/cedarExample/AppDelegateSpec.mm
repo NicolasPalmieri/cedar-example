eea0be10f01c376c07c526f07210d0978f217f6a
Nicolás Palmieri <npalmieri@humana.com>
2016-04-15 11:24:29 -0400

    added dependency: cedar

---
+++ cedarExample/cedarExample/AppDelegateSpec.mm
@@ -0,0 +1,17 @@
+#import <Cedar/Cedar.h>
+#import "AppDelegate.h"
+
+using namespace Cedar::Matchers;
+using namespace Cedar::Doubles;
+
+SPEC_BEGIN(AppDelegateSpec)
+
+describe(@"AppDelegate", ^{
+    __block AppDelegate *subject;
+
+    beforeEach(^{
+
+    });
+});
+
+SPEC_END
---
+++ cedarExample/cedarExample/ViewControllerSpec.mm
@@ -0,0 +1,17 @@
+#import <Cedar/Cedar.h>
+#import "ViewController.h"
+
+using namespace Cedar::Matchers;
+using namespace Cedar::Doubles;
+
+SPEC_BEGIN(ViewControllerSpec)
+
+describe(@"ViewController", ^{
+    __block ViewController *subject;
+
+    beforeEach(^{
+
+    });
+});
+
+SPEC_END
